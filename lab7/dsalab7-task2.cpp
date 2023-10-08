0fafef8fecdf985b152cfff0fc50129bb9fbd0fb
himanshu-skid19 <h.singhal@iitg.ac.in>
2023-10-08 15:55:31 +0530

    finished with bfs

--- lab7/dsalab7-task2.cpp
+++ lab7/dsalab7-task2.cpp
@@ -1,9 +1,89 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 struct Vertex {
     int data;
     Vertex* next;
+    string color;
+    long long d;
+    Vertex *parent;
+};
+
+class Node {
+	public:
+		Node *next;
+		int data;
+
+		Node(){
+			data = NULL;
+			next = NULL;
+
+		}
+
+		Node(int data) {
+		this->data = data;
+		this->next = NULL;
+		}
+};
+
+class queue {
+	Node *head;
+	Node *tail;
+	public:
+		queue() {head = NULL; tail == NULL;}
+
+		int IsEmpty(){
+			if (head == NULL){
+				return 0;
+			}
+			return 1;
+		}
+
+		void enqueue(int data){
+			Node *new_node = new Node(data);
+			if (head == NULL){
+				head = new_node;
+				return;
+			}
+			Node *temp = head;
+			while (temp->next != NULL){
+				temp = temp->next;
+			}
+			temp->next = new_node;
+			tail = new_node;
+			return;
+
+		}
+
+		void printqueue() {
+			Node *curr = head;
+
+			if (head == NULL) {
+				cout << "queue is empty\n";
+				return;
+			}
+			while (curr != NULL) {
+				cout << curr->data << " ";
+				curr = curr->next;
+			}
+			cout << "\n";
+		}
+
+		int dequeue(){
+			if (head == NULL) {cout << "The queue is empty\n"; return -1;}
+			Node *temp = head;
+			head = head->next;
+			return temp->data;
+		}
+
+		int front(){
+			if (head == NULL){
+				printf("Queue is empty\n");
+				return 0;
+			}
+			return head->data;
+		}
 };
 
 class Graph {
@@ -69,7 +149,7 @@ public:
         adjList[head] = newNode;
         head++;
     }
-    
+
    void deleteVertex(int data){
         int j = get_index_from_adjlist(data);
         if (j == -1){
@@ -169,60 +249,110 @@ public:
     }
 
 
+    void BFS(int data){
+        Graph g2(numVertices);
+
+        int i = get_index_from_adjlist(data);
+        for (int j = 0; j < head; j++){
+            Vertex *curr = adjList[j];
+            while (curr != nullptr){
+            curr->color = "WHITE";
+            curr->d = 1e18;
+            curr->parent = NULL;
+            curr=curr->next;
+
+            }
+        }
+        adjList[i]->color = "GRAY";
+        adjList[i]->d = 0;
+        adjList[i]->parent = NULL;
+
+        queue q;
+        q.enqueue(adjList[i]->data);
+        while (q.IsEmpty() != 0){
+            int u = q.dequeue();
+            int idx = get_index_from_adjlist(u);
+            Vertex *curr = adjList[idx];
+            while (curr != nullptr){
+                if (curr->color == "WHITE" && curr->color != "BLACK"){
+
+                    int x = get_index_from_adjlist(curr->data);
+                    for (int z = 0; z<numVertices; z++){
+                        Vertex *temp = adjList[z];
+                        while (temp != nullptr){
+                            if (temp->data == curr->data){
+                                temp->color = "GRAY";
+                                temp->d = adjList[idx]->d + 1;
+                                temp->parent = adjList[idx];
+
+                            }
+                            temp = temp->next;
+                        }
+                    };
+                    q.enqueue(curr->data);
+                }
+
+                curr = curr->next;
+            }
+
+            for (int z = 0; z<head; z++){
+                Vertex *temp = adjList[z];
+                while (temp != nullptr){
+                    if (temp->data == u){
+                        temp->color = "BLACK";
+
+                    }
+                    temp = temp->next;
+                }
+            }
+        }
+
+        cout << "Vertex : Minimum Distance\n";
+        for (int i = 0; i < head; i++){
+            cout << adjList[i] -> data << " : " <<  adjList[i]->d << endl;
+        }
+    }
+
 };
 
 int main(void){
-    cout << "Welcome to this program, how many vertices do you want your graph to have? \n";
-    int num;
-    cin >> num;
-    Graph graph(num);
+    Graph graph(10);
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+    graph.addVertex(5);
+    graph.addVertex(6);
+    graph.addVertex(7);
+    graph.addVertex(8);
+    graph.addVertex(9);
 
-    int input;
-	while (input != 6){
-		cout << "Enter 1 to add a vertex to the graph\n";
-		cout << "Enter 2 to add an edge between two vertices\n";
-		cout << "Enter 3 to delete a vertex from the graph\n";
-		cout << "Enter 4 to delete a edge between two vertices\n";
-		cout << "Enter 5 to print the adjacency list representing the graph\n";
-		cout << "Enter 6 to exit the program\n";
-		cout << "What do you want to do? \n";
-		cin >> input;
-		switch (input){
-			case 1:
-				int val;
-				cout << "Enter the value of the vertex: \n";
-				cin >> val;
-				graph.addVertex(val);
-				break;
-			case 2:
-				int m;
-                int n;
-                cout << "Enter the value of the first vertex: \n";
-				cin >> m;
-                cout << "Enter the value of the second vertex: \n";
-				cin >> n;
-				graph.addEdge(m, n);
-				break;
-			case 3:
-				int key;
-				cout << "Enter the value of the vertex you want to delete: \n";
-				cin >> key;;
-				graph.deleteVertex(key);
-				break;
-			case 4:
-				int x;
-                int y;
-                cout << "Enter the value of the first vertex: \n";
-				cin >> x;
-                cout << "Enter the value of the second vertex: \n";
-				cin >> y;
-				graph.deleteEdge(x, y);
-				break;
-			case 5:
-				graph.printGraph();
-				break;
-		}
-	}
+    graph.addEdge(1,2);
+    graph.addEdge(1,3);
+    graph.addEdge(2,3);
+    graph.addEdge(2,9);
+    graph.addEdge(3,4);
+    graph.addEdge(3,6);
+    graph.addEdge(4,5);
+    graph.addEdge(4,9);
+    graph.addEdge(5,6);
+    graph.addEdge(5,8);
+    graph.addEdge(6,7);
+    graph.addEdge(7,8);
+    graph.addEdge(8,9);
+
+    graph.printGraph();
+
+    graph.BFS(5);
+
+
+
+
+
+
+
+
+
 
 
 }
