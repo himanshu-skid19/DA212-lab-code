a48095734de463c49beb6c367992145a42679cee
Himanshu <h.singhal@iitg.ac.in>
2023-10-23 01:27:09 +0530

    done with the bare minimum

--- lab8/selection_sort.cpp
+++ lab8/selection_sort.cpp
@@ -12,20 +12,25 @@
 using namespace std;
 using namespace std::chrono;
 
-void insertion_sort(int *A, int n){
-	for (int i = 1; i < n+1; i++){
-		int key = A[i];
-		int j = i-1;
-		while (j >= 0 && A[j] > key){
-			A[j+1] = A[j];
-			j--;
-
+void selection_sort(int *A, int n){
+	for (int i = 0; i < n; i++){
+		int min = 100000000;
+		int j=i;
+		int idx=0;
+		for (j = i; j < n; j++){
+			if (A[j] <= min){
+				min = A[j];
+				idx = j;
+			}
 		}
-		A[j+1] = key;
+		int temp = A[idx];
+		A[idx] = A[i];
+		A[i] = temp;
 	}
 }
 
-void test_insertion_sort(){
+
+void test_selection_sort(){
 	int n = 10;
 	while (n <= 1000000000){
 		int* list = new int[n];
@@ -33,19 +38,22 @@ void test_insertion_sort(){
 		srand(time(NULL));
 		for(i=0; i<n; i++)
 			list[i] = rand()%1000;
+
+
 		auto start = high_resolution_clock::now();
-		insertion_sort(list, n);
+		selection_sort(list, n);
 		auto stop = high_resolution_clock::now();
 		auto duration = duration_cast<nanoseconds>(stop-start);
 
 
 		cout << "Time taken by insertion_sort algorithm to sort a array of length " << n << " is given by " << duration.count()*1e-9 << " seconds "<< "\n";
-		cout << "\n";
+
 		n = n*10;
 
 
 	}
 }
+
 
 void test_sorting(){
     int n = 10;
@@ -81,7 +89,7 @@ int main(void){
 		cin >> input;
 		switch (input){
 			case 1:
-				test_insertion_sort();
+				test_selection_sort();
 				break;
 			case 2:
                 test_sorting();
@@ -89,6 +97,4 @@ int main(void){
 
 		}
 	}
-
-
 }
