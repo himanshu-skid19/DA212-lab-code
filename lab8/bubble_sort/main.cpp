73d69ea8e627dce193692f8bcdc88e8dcaa7f384
Himanshu <h.singhal@iitg.ac.in>
2023-10-25 00:30:39 +0530

    forgot about bubble sort lol

---
+++ lab8/bubble_sort/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "bubble_sort.h"
+#include "sorting.h"
+#include "run.h"
+
+
+
+int main(void) {
+    run();
+    return 0;
+}
---
+++ lab8/bubble_sort/run.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "bubble_sort.h"
+#include "sorting.h"
+
+
+using namespace std;
+
+void run(){
+
+    cout << "WARNING: It might take a very long time for a specific algorithm to sort an array so be on a lookout for that.\n";
+    int input = -1;
+    while (input != 3){
+		cout << "Enter 1 to test on my implementation of bubble sort \n";
+		cout << "Enter 2 to test on STL's implementation of sorting'\n";
+		cout << "Enter 3 to exit the program\n";
+
+		cin >> input;
+		switch (input){
+			case 1:
+				test_bubble_sort();
+				break;
+			case 2:
+                test_sorting();
+				break;
+
+		}
+
+    }
+}
